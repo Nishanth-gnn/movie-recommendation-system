d2b4296734e91707b05e2fb3f7e578ea187db447
Goppu Narayana Nishanth <gnishanth2005@gmail.com>
2025-10-16 15:55:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pickle
 import streamlit as st
 import requests
+import zipfile
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
@@ -12,6 +13,9 @@ def fetch_poster(movie_id):
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
+    with zipfile.ZipFile('similarity.ZIP', 'r') as zip_ref:
+        with zip_ref.open('similarity.pkl') as file:
+            similarity = pickle.load(file)
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
     recommended_movie_posters = []
@@ -23,13 +27,11 @@ def recommend(movie):
 
     return recommended_movie_names,recommended_movie_posters
 
-
 st.header('Movie Recommender System')
 import pandas as pd
 
 movies_dict = pickle.load(open('movies_dict.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl','rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
@@ -46,7 +48,6 @@ if st.button('Show Recommendation'):
     with col2:
         st.text(recommended_movie_names[1])
         st.image(recommended_movie_posters[1])
-
     with col3:
         st.text(recommended_movie_names[2])
         st.image(recommended_movie_posters[2])
